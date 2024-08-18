fce24574bbcd6f52d2fb56af5f46f5c175170b62
Choaib ELMADI <choaib3elmadi@gmail.com>
2024-08-18 12:37:48 +0100

    Authentication implementation with Clerk

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,5 +1,13 @@
+"use client";
+
 import { Editor } from "@/components/editor/Editor";
 import Header from "@/components/Header";
+import {
+	SignInButton,
+	UserButton,
+	SignedOut,
+	SignedIn,
+} from "@clerk/clerk-react";
 import React from "react";
 
 const Document = () => {
@@ -7,8 +15,14 @@ const Document = () => {
 		<div>
 			<Header>
 				<div className="flex w-fit items-center justify-center gap-2">
-					<p className="document-title">Untitled</p>
+					<p className="document-title">Share</p>
 				</div>
+				<SignedOut>
+					<SignInButton />
+				</SignedOut>
+				<SignedIn>
+					<UserButton />
+				</SignedIn>
 			</Header>
 			<Editor />
 		</div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import { Inter as FontSans } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 import { cn } from "@/lib/utils";
 import { Metadata } from "next";
 import React from "react";
@@ -21,12 +23,22 @@ export default function RootLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<html lang="en" suppressHydrationWarning>
-			<body
-				className={cn("min-h-screen font-sans antialiased", fontSans.variable)}
-			>
-				{children}
-			</body>
-		</html>
+		<ClerkProvider
+			appearance={{
+				baseTheme: dark,
+				variables: { colorPrimary: "#3371FF", fontSize: "16px" },
+			}}
+		>
+			<html lang="en" suppressHydrationWarning>
+				<body
+					className={cn(
+						"min-h-screen font-sans antialiased",
+						fontSans.variable
+					)}
+				>
+					{children}
+				</body>
+			</html>
+		</ClerkProvider>
 	);
 }
