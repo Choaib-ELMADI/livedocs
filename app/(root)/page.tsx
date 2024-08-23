2a70efa05f4914e1f95ceee2c25ac1ee109fcbc3
Choaib ELMADI <choaibamd@gmail.com>
2024-08-23 22:42:43 +0100

    List all the created documents

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,16 +1,21 @@
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { getDocuments } from "@/lib/actions/room.actions";
 import AddDocumentBtn from "@/components/AddDocumentBtn";
 import { currentUser } from "@clerk/nextjs/server";
+import { dateConverter } from "@/lib/utils";
+import { redirect } from "next/navigation";
 import Header from "@/components/Header";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
-import { redirect } from "next/navigation";
 
 const Home = async () => {
 	const clerkUser = await currentUser();
 	if (!clerkUser) redirect("/sign-in");
 
-	const documents = [];
+	const documents = await getDocuments(
+		clerkUser.emailAddresses[0].emailAddress
+	);
 
 	return (
 		<main className="home-container">
@@ -25,8 +30,43 @@ const Home = async () => {
 					</SignedOut>
 				</div>
 			</Header>
-			{documents.length > 0 ? (
-				<div>items</div>
+			{documents.data.length > 0 ? (
+				<div className="document-list-container">
+					<div className="document-list-title">
+						<h3 className="text-2xl font-semibold">All documents</h3>
+						<AddDocumentBtn
+							userId={clerkUser.id}
+							email={clerkUser.emailAddresses[0].emailAddress}
+						/>
+					</div>
+					<ul className="document-ul">
+						{documents.data.map((doc: any) => (
+							<li key={doc.id} className="document-list-item">
+								<Link
+									href={`/documents/${doc.id}`}
+									className="flex flex-1 items-center gap-4"
+								>
+									<div className="hidden rounded-md bg-dark-500 p-2 sm:block">
+										<Image
+											src="/assets/icons/doc.svg"
+											alt="Document"
+											width={40}
+											height={40}
+											draggable="false"
+										/>
+									</div>
+									<div className="space-y-1">
+										<p className="line-clamp-1 text-lg">{doc.metadata.title}</p>
+										<p className="text-sm font-light text-blue-100">
+											Created about {dateConverter(doc.createdAt)}
+										</p>
+									</div>
+								</Link>
+								{/* TODO: Add a delete button */}
+							</li>
+						))}
+					</ul>
+				</div>
 			) : (
 				<div className="document-list-empty">
 					<Image
--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -77,3 +77,12 @@ export const updateDocument = async ({
 		console.log(`Error occured while updating the document: ${error}`);
 	}
 };
+
+export const getDocuments = async (email: string) => {
+	try {
+		const rooms = await liveblocks.getRooms({ userId: email });
+		return parseStringify(rooms);
+	} catch (error) {
+		console.log(`Error occured while getting documents: ${error}`);
+	}
+};
